ca73f0e8652df7734490e3d29a59f185d030e3a8
madhurmoms <madhurmoms@gmail.com>
2023-05-13 11:13:28 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
 
   return (
     <>
-      <Header title="Madhur" />
+      <Header title="MyTodosList" />
       <Todos todos={todos} onDelete={onDelete} />
       <Footer />
     </>
