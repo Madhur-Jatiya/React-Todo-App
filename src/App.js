bb168288b2eadf3adf7ff4f06224c60b892fba8e
madhurmoms <madhurmoms@gmail.com>
2023-05-13 10:40:09 +0530

    delete

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
   ]);
   return (
     <>
-      <Header title="Madhur" />
-      <Todos todos={todos} onDelete={onDelete} />
+      <Header title="MyTodo" />
+      <Todos todos={todos} onDelete={onDelete} /> 
       <Footer />
     </>
   );
